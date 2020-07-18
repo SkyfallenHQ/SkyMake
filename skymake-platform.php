4afb6ceff450f1ea299935925570cff98ca5c4fc
Yiğit Kerem Oktay <63040646+yigitkeremoktay@users.noreply.github.com>
2020-07-18 15:09:07 +0300

    We've made every single button work!

--- skymake-platform.php
+++ skymake-platform.php
@@ -17,7 +17,7 @@ if(substr( $request, 0, 7 ) === "lesson/") {
     if (!($lessonname == "n")) {
         echo("<div class='text-center'><h1>Lesson Details | " . $lessonname . "</h1></div>");
         $n = getassignedlessonquery($link, $cenroller);
-        echo(overview(getassignedlessons($link)[$n], getassignedteachers($link)[$n], getassignedtimes($link)[$n], getassignedtopics($link)[$n], getassignedunits($link)[$n], getassignedbgurls($link)[$n],getassignedids($link)[$n],"<p>No content assigned.</p>"));
+        echo(overview(getassignedlessons($link)[$n], getassignedteachers($link)[$n], getassignedtimes($link)[$n], getassignedtopics($link)[$n], getassignedunits($link)[$n], getassignedbgurls($link)[$n],getassignedids($link)[$n],"<p>No content can be assigned as it is not supported by this version of SkyMake 4 Beta.</p>"));
     }else {
         echo("<div class='text-center'><h1>This lesson does not exist. Please access your course by dashbboard.</h1></div>");
     }
