38f09ddc34d2a26caa0714b8630dac61f936cb85
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-08-25 23:08:03 +0300

    Trying fixes

--- classes/user.php
+++ classes/user.php
@@ -1,22 +1,18 @@
 <?php
     function getRole($link,$username){
-        $sql = "SELECT `role` FROM skymake_roles WHERE username='".$username."'";
+        $sql = "SELECT * FROM skymake_roles WHERE username=\"".$username."\";";
         if($result = mysqli_query($link, $sql)){
             if(mysqli_num_rows($result) == 1){
                 while($row = mysqli_fetch_array($result)){
-                    $role = $row['role'];
-                    mysqli_free_result($result);
+                    return $row['role'];
                 }
             }
-            else {
-                $role = "unverified";
+            else{
+                return "unverified";
             }
-        }
-        else{
+        }else{
             die("ERROR: Could not able to execute $sql. " . mysqli_error($link));
         }
-        mysqli_close($link);
-        return $role;
     }
     function setRole($link,$username,$newrole){
         $isroleadded = false;
