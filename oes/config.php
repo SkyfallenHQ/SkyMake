18e72a2a681ab5ca370131d126fe4d88c119d915
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-08-27 18:22:34 +0300

    OE System Integration

--- oes/config.php
+++ oes/config.php
@@ -1,5 +1,5 @@
 <?php
-require_once "../SkyMakeDatabaseConnector/SkyMakeDBConfig.php";
+require_once "../SkyMakeDatabaseConnector/SkyMakeDBconfig.php";
 session_name('SkyMakeSessionStorage');
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
