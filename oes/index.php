af69637809df7a89eac3102fb3ae475250bd8fe6
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-08-27 18:25:05 +0300

    OE System Integration

--- oes/index.php
+++ oes/index.php
@@ -123,7 +123,7 @@ if (isset($_POST["backbtn"])){
     </div>
     <form method="post">
         <img src="<?php
-        $sql = "SELECT FROM skymake_qanswer (picurl) WHERE examid='".$_SESSION["examid"]."' and qn='".$_SESSION["qn"]."'";
+        /*$sql = "SELECT FROM skymake_qanswer (picurl) WHERE examid='".$_SESSION["examid"]."' and qn='".$_SESSION["qn"]."'";
         if($result = mysqli_query($link, $sql)){
             if(mysqli_num_rows($result) == 0){
                 while($row = mysqli_fetch_array($result)){
@@ -137,6 +137,8 @@ if (isset($_POST["backbtn"])){
             die("ERROR: Could not able to execute $sql. " . mysqli_error($link));
         }
         mysqli_close($link);
+        */
+        echo "test.jpg";
         ?>">
      <?php echo "Question Number:".$_SESSION["qn"]." Out of: ".$examdata["exam_qcount"]; ?><br>
             <input name="backbtn" type="submit" class="btn btn-primary" value="Don't Submit and Go Back">
