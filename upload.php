17520f11b089f019ab5461e2bac77e2ccc712bf9
Yiğit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-08-28 21:31:58 +0300

    Add useruploads

--- upload.php
+++ upload.php
@@ -1,5 +1,7 @@
 <?php
 include "nps/widgets/dash.php";
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 $target_dir = "UserUploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
