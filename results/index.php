0971dd627d076300f8377c29e9a3dc862a51c9dd
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-08-28 13:59:35 +0300

    Fixing OES

--- results/index.php
+++ results/index.php
@@ -1,24 +1,57 @@
 <?php
 require_once "config.php";
+include "../nps/widgets/dash.php";
 session_start();
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: /");
     exit;
 }
-// Check connection
+if(!isset($_GET["examid"])){
+    header("/");
+}
+$sql = "SELECT exam_name,exam_start,exam_end,exam_qcount,exam_type FROM skymake_examdata WHERE examid='".$_SESSION["examid"]."'";
+if($result = mysqli_query($link, $sql)){
+    if(mysqli_num_rows($result) == 1){
+        while($row = mysqli_fetch_array($result)){
+            $examdata["exam_name"] = $row["exam_name"];
+            $examdata["exam_start"] = $row["exam_start"];
+            $examdata["exam_end"] = $row["exam_end"];
+            $examdata["exam_qcount"] = $row["exam_qcount"];
+            $examdata["exam_type"] = $row["exam_type"];
+        }
+        mysqli_free_result($result);
+    } else{
+        echo "Exam invalid. You will be redirected in 3 seconds";
+        sleep(3);
+        header("location: /");
+    }
+} else{
+    die("ERROR: Could not able to execute $sql. " . mysqli_error($link));
+}
+if (new DateTime() < new DateTime($examdata["exam_end"])) {
+    echo "Your time is not over. \n";
+    echo "Your results won't be shown..\n";
+    echo "Redirecting in 10 seconds \n";
+    sleep(10);
+    header("location: /");
+}
+$_SESSION["examid"] = $_GET["examid"];
 $link2 = $linktwo;
-$sql = "SELECT qn,answer FROM skymake_answer WHERE id=".$_SESSION["id"].";";
-$sql2 = "SELECT answer FROM skymake_answer WHERE id=3;";
+$sql = "SELECT qn,answer FROM skymake_answer WHERE id=".$_SESSION["id"]." and examid='".$_SESSION["examid"]."'";
+$sql2 = "SELECT answer FROM skymake_qanswers WHERE examid='".$_SESSION["examid"]."'";
 if ($res = mysqli_query($link, $sql)) { 
     if ($res2 = mysqli_query($link2, $sql2)) { 
-    if (mysqli_num_rows($res) == 90 and mysqli_num_rows($res2) == 90) { 
-        echo "<table>"; 
+    if (mysqli_num_rows($res) == $examdata["exam_qcount"] and mysqli_num_rows($res2) == $examdata["exam_qcount"]) {
+        echo "<table class='table'>";
+        echo "<thead>";
         echo "<tr>"; 
-        echo "<th>Question Nr.</th>"; 
-        echo "<th>Your Answer</th>"; 
-        echo "<th>Right Answer</th>"; 
-        echo "</tr>"; 
+        echo "<th scope='col'>Question Nr.</th>";
+        echo "<th scope='col'>Your Answer</th>";
+        echo "<th scope='col'>Right Answer</th>";
+        echo "</tr>";
+        echo "</thead>";
+        echo "<tbody>";
         $qn_internal = 0;
         $m1 = 0;
         $m2 = 0;
@@ -28,9 +61,10 @@ if ($res = mysqli_query($link, $sql)) {
             $row2 = mysqli_fetch_array($res2);
             $qn_internal++;
             echo "<tr>"; 
-            echo "<td>".$row['qn']."</td>"; 
+            echo "<th scope='row'>".$row['qn']."</th>";
             echo "<td>".$row['answer']."</td>";
             echo "<td>".$row2['answer']."</td>";
+            echo "</tr>";
             if($examtype="90withMSL") {
                 if ($row['answer'] != $row2['answer']) {
                     if ($row['answer'] == "Q_EMPTY") {
@@ -62,7 +96,7 @@ if ($res = mysqli_query($link, $sql)) {
                 }
             }
         }
-
+        echo "</tbody>";
         echo "</table><p>".$points."</p>"; 
                 $sql = "INSERT INTO skymake_result (un, p) VALUES ('".$_SESSION["username"]."','".$points."');";
         if (mysqli_query($link, $sql)) { 
