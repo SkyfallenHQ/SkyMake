55ebf1ba84c3aac1dea534514c081a97b3f5384e
Yiğit Kerem Oktay <yigit.oktay01@dogakoleji.com.tr>
2020-09-17 15:06:20 +0300

    Update install.sql

--- install.sql
+++ install.sql
@@ -1,28 +1,17 @@
--- phpMyAdmin SQL Dump
--- version 4.9.5
--- https://www.phpmyadmin.net/
+-- SkyMake 4 - SQL Installation Script
+-- version 2.5.3
+-- https://thesf.me/linkto-skymake4
 --
--- Host: localhost
--- Generation Time: Aug 30, 2020 at 05:38 PM
--- Server version: 5.7.31-0ubuntu0.18.04.1
--- PHP Version: 7.2.24-0ubuntu0.18.04.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `theskyfallen_skymake-preproduction`
---
-
--- --------------------------------------------------------
 
 --
 -- Table structure for table `skymake_answer`
