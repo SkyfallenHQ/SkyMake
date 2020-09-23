5179edd195efe6b83599be626a498144a2822194
Yiğit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-09-23 10:37:49 +0300

    Update skymake-platform.php

--- skymake-platform.php
+++ skymake-platform.php
@@ -241,26 +241,13 @@ if($_POST['elm1']!='') {
 ?>
         <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5.4.2-90/tinymce.min.js" referrerpolicy="origin"></script>
 <script language="javascript" type="text/javascript">
-    tinyMCE.init({
-        theme : "advanced",
-        mode: "exact",
-        elements : "elm1",
-        theme_advanced_toolbar_location : "top",
-        theme_advanced_buttons1 : "bold,italic,underline,strikethrough,separator,"
-            + "justifyleft,justifycenter,justifyright,justifyfull,formatselect,"
-            + "bullist,numlist,outdent,indent",
-        theme_advanced_buttons2 : "link,unlink,anchor,image,separator,"
-            +"undo,redo,cleanup,code,separator,sub,sup,charmap",
-        theme_advanced_buttons3 : "",
-        height:"350px",
-        width:"600px"
-    });
+    tinymce.init({selector:'textarea'});
 
 </script>
 <?php echo $sHeader;?>
 <h2>Post to Board</h2>
 <form method="post" action="<?=$_SERVER['REQUEST_URI']?>">
-    <textarea id="elm1" name="elm1" rows="15" cols="80"><?php echo $sContent;?></textarea>
+    <textarea id="posttoboard" name="posttoboard" rows="15" cols="80"><?php echo $sContent;?></textarea>
     <br />
     <input type="submit" name="save" value="Submit" />
     <input type="reset" name="reset" value="Reset" />
