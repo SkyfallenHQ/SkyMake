7b7720dd4c20dbfbe4f33ae6f3fab713c6d3e651
Yiğit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-09-23 10:27:25 +0300

    Fix crash function

--- SkyMakeConfiguration.php
+++ SkyMakeConfiguration.php
@@ -2,19 +2,5 @@
 define("SkyMakeOnConfigConnect", "CONFCONNOK");
 //Start editing here!
 define("SFLC_HOST","meet.jit.si");
-include "SkyMakeFunctionSet/includes.php";
 // That's all. Now stop.
-
-/****************************************************************/
-//                 Skyfallen Security Check
-//    This will check if the software update server is secure
-//       as it can be a threat while updating the software
-/****************************************************************/
-crashSkyMake("TestCrash","ErrorReason");
-
-/* $gcret = MCS::getCertificate();
-if($gcret["success"] == true){
-    MCS::log("Certificate downloaded successfully.");
-}else{
-
-} */
+include "SkyMakeFunctionSet/includes.php";
