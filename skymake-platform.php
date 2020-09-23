96cd4e12a6bcb6eccdafc0740e99546af30b2431
Yiğit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-09-23 10:33:01 +0300

    Update skymake-platform.php

--- skymake-platform.php
+++ skymake-platform.php
@@ -223,6 +223,50 @@ if($user_role == "teacher") {
         include "nps/widgets/dash.php";
         echo("<a href='/logout'><h1>Log Out</h1></a>");
     }
+    if ($request == "board" or $request == "board/") {
+        $requestsuccess = true;
+        include "nps/widgets/dash.php";
+$allowedTags='<p><strong><em><u><h1><h2><h3><h4><h5><h6><img>';
+$allowedTags.='<li><ol><ul><span><div><br><ins><del>';
+// Should use some proper HTML filtering here.
+if($_POST['elm1']!='') {
+    $sHeader = '<h1>Ah, content is king.</h1>';
+    $sContent = strip_tags(stripslashes($_POST['elm1']),$allowedTags);
+} else {
+    $sHeader = '<h1>Nothing submitted yet</h1>';
+    $sContent = '<p>Start typing...</p>';
+    $sContent.= '<p><img width="107" height="108" border="0" src="/mediawiki/images/badge.png"';
+    $sContent.= 'alt="TinyMCE button"/>This rover has crossed over</p>';
+}
+?>
+        <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+<script language="javascript" type="text/javascript">
+    tinyMCE.init({
+        theme : "advanced",
+        mode: "exact",
+        elements : "elm1",
+        theme_advanced_toolbar_location : "top",
+        theme_advanced_buttons1 : "bold,italic,underline,strikethrough,separator,"
+            + "justifyleft,justifycenter,justifyright,justifyfull,formatselect,"
+            + "bullist,numlist,outdent,indent",
+        theme_advanced_buttons2 : "link,unlink,anchor,image,separator,"
+            +"undo,redo,cleanup,code,separator,sub,sup,charmap",
+        theme_advanced_buttons3 : "",
+        height:"350px",
+        width:"600px"
+    });
+
+</script>
+<?php echo $sHeader;?>
+<h2>Post to Board</h2>
+<form method="post" action="<?=$_SERVER['REQUEST_URI']?>">
+    <textarea id="elm1" name="elm1" rows="15" cols="80"><?php echo $sContent;?></textarea>
+    <br />
+    <input type="submit" name="save" value="Submit" />
+    <input type="reset" name="reset" value="Reset" />
+</form>
+        <?php
+    }
     if ($request == "logout" or $request == "logout/") {
         $requestsuccess = true;
         include "nps/widgets/dash.php";
