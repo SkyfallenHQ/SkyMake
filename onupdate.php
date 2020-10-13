b8a7b173e3c99043c281ff4930fce0998ead6898
Yiğit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-10-13 14:46:39 +0300

    DEV204001

--- onupdate.php
+++ onupdate.php
@@ -1,2 +1,13 @@
 <?php
 // This code will be executed when SkyMake is updated to a specific package and then be deleted.
+include_once "SkyMakeDatabaseConnector/SkyMakeDBconfig.php";
+$val = mysqli_query($link,'select 1 from `skymake_board` LIMIT 1');
+
+if($val !== FALSE)
+{
+    echo "DB already created. Completing update...";
+}
+else
+{
+    $sql = "CREATE TABLE `skymake_board` ( `entry` INT NOT NULL AUTO_INCREMENT , `classid` VARCHAR(255) NOT NULL , `sender` VARCHAR(255) NOT NULL , `message` VARCHAR(255) NOT NULL , UNIQUE (`entry`)) ENGINE = InnoDB;";
+}
