b7410f0142efff879af7e5ebbc3ec3530924bb79
Yiğit Kerem Oktay <yigit.oktay01@dogakoleji.com.tr>
2020-10-16 17:53:52 +0300

    Update skymake-platform.php

--- skymake-platform.php
+++ skymake-platform.php
@@ -8,8 +8,6 @@ include "SkyMakeFunctionSet/Operation-Requirements/MainFunctions.php";
 include "SkyMakeDatabaseConnector/SkyMakeDBconfig.php";
 include "classes/user.php";
 include "SkyMakeDatabaseConnector/db-class.php";
-include "SkyMakeConfiguration.php";
-session_name('SkyMakeSessionStorage');
 session_start();
 if(!$_SESSION["loggedin"]){
     header("Location: /?act=signin");
